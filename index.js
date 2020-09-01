0bf8de4d407c827f0a795781acd4b6ea80761a77
Andres Daza <christiandaza@gmail.com>
2020-08-31 22:21:56 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,69 @@
+/*
+//ejercicio 1
+alert("hello world");
+
+
+//ejercicio 2
+document.write("ejercicio 2"+"<br/>");
+document.open();
+document.write("hello world<br/>");
+document.close();
+
+//ejercicio 3
+document.write("ejercicio 3"+"<br/>");
+document.write(3+5+"<br/>");
+
+//ejercicio 4
+document.write("ejercicio 4"+"<br/>");
+var nombre = prompt("Digite su nombre");
+document.write("hola "+nombre+"<br/>");
+
+//ejercicio 5
+document.write("ejercicio 5"+"<br/>");
+var num1 = prompt("Digite numero 1");
+var num2 = prompt("Digite numero 2");
+var resultado = (parseInt(num1))+(parseInt(num2));
+document.write(`El resultado es: ${resultado}`+"<br/>");
+
+document.write("ejercicio 6"+"<br/>");
+//ejercicio 6
+document.write("ejercicio 6"+"<br/>");
+var num1 = prompt("Digite numero 1");
+var num2 = prompt("Digite numero 2");
+
+if(num1 > num2){
+    document.write("El numero mayor es: "+num1);
+}else{
+    document.write("El numero mayor es: "+num2)
+}
+
+//ejercicio 7
+document.write("ejercicio 7"+"<br/>");
+var num1 = prompt("Digite numero 1");
+var num2 = prompt("Digite numero 2");
+var num3 = prompt("Digite numero 3");
+
+if(num1 > num2 ){
+    if(num1 > num3){
+        document.write("El numero mayor es: "+num1);
+    }else{
+        document.write("El numero mayor es: "+num3);
+    }
+
+}else if(num2> num3){
+    document.write("El numero mayor es: "+num2);
+    }else{
+        document.write("El numero mayor es: "+num3);
+    }
+*/    
+//ejercicio 8
+document.write("ejercicio 8"+"<br/>");
+var num1 = prompt("Digite numero 1");
+
+var mod = num1 % 2;
+if(mod == 0){
+    document.write("El número: "+num1+" es divisible por 2");
+}else{
+    document.write("El número: "+num1+" NO es divisible por 2");
+}
+
